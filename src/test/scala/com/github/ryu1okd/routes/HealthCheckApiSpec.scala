c2c668081d1b92b1c6938001be1db7f35e59ae8f
ryu1okd <r122.project@gmail.com>
2018-01-14 15:36:26 +0900

    Add status check for health_check api.

--- src/test/scala/com/github/ryu1okd/routes/HealthCheckApiSpec.scala
+++ src/test/scala/com/github/ryu1okd/routes/HealthCheckApiSpec.scala
@@ -1,13 +1,16 @@
 package com.github.ryu1okd.routes
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
 
 class HealthCheckApiSpec extends WordSpec with Matchers with ScalatestRouteTest with HealthCheckRoutes {
 
   "HealthCheckRoutes " should {
     "when colling GET '/health_check' should {status: ok}" in {
       Get("/health_check") ~> healthCheckRoutes ~> check {
+        status should equal(StatusCodes.OK)
         responseAs[String] shouldEqual "{\"status\":\"ok\"}"
       }
     }
